47f94a0066602a45bd8f8e1dec93e1a177464734
Controleur <18554067+controleur@users.noreply.github.com>
2025-05-01 14:25:22 +0300

    mise en forme page NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,18 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Link } from "react-router";
+import "../styles/NotFound.sass";
 function NotFound() {
-    return (
-      <>
+  return (
+    <>
       <Header />
-      <h1>404</h1>
+      <main className="notFound">
+        <h1>404</h1>
+        <p>Oups ! La page que vous demandez n'existe pas.</p>
+        <Link to="/">Retourner sur la page d'accueil</Link>
+      </main>
       <Footer />
-      </>
-      
-    );
-  }
-  export default NotFound;
+    </>
+  );
+}
+export default NotFound;
